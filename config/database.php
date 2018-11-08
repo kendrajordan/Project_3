47eaa7a0a65dad76dc4f1a91f452e7bdd3b006a0
Kendra Jordan <kendra.jordan8@gmail.com>
2018-11-08 16:21:26 -0500

    commented out line 70 and changed the sslmode to required

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
             'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
+          //  'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [

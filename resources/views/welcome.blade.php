16250896365ce57e62f6a01b26fe995a946b76d3
Kendra Jordan <kendra.jordan8@gmail.com>
2018-11-08 23:03:58 -0500

    added buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 
-@extends('profile.layout')
+@extends('layouts.app')
 
 @section('content')
 
@@ -9,13 +9,13 @@
             <div class='card mb-3 mt-3 pr-0 pl-0 container bg-dark text-white-50'>
 
                 <div class="card-header font-weight-bold">{{$post->title}}</div>
-                <div>
-                  <a href='/profile/edit'><i class="fas fa-edit"></i></a>
-                </div>
                 <div class='card-body p-0'>
                 <a href="{{$post->link}}">Link</a>
                 <p class="card-text">{{$post->text}}</p>
                 <p class="card-text">Posted by {{$post->user->name}}</p>
+                <div class="row">
+                  <button type="button" class="bg-success">Edit</button><button type="button" class="bg-danger">DELETE</button>
+                </div>
                 <div class="card-footer">
                   <small class="text-muted">Last updated {{$post->updated_at}}</small>
                 </div>
